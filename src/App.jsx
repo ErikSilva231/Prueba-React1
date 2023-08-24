f5136d5e2423815fe916c1b6e4c1fdb7a4544f0a
ErikSilva231 <erik.silva.fuentes@gmail.com>
2023-08-24 00:39:08 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,44 +2,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 
 function App() {
- /* const [userInput, setUserInput] = useState("")
-
-  const handleChange = (e) => {
-    setUserInput (e.target.value)
-  }
-
-  useEffect(() => {
-    // se ejecuta al comienzo y cada vez que se cambia
-    console.log(userInput)
-  }, [userInput]);
-  //estado de dependencias, el console log se ejecuta cada vez que se actualiza el userInput, se puede poner mas de una dependencias ej [userInpuit,userPassword]
-
-  return (
-    <div>
-   <input onChange={handleChange} />
-    </div>
-  );
-}*/
-
-const [characterName , setCharacterName] = useState ("");
-const [characterSentence , setCharacterSentence] = useState ("");
-characterSentence
+const [ indicadorNombre , setIndicadorNombre] = useState ("");
+const [ indicadorFecha, setIndicadorFecha] = useState ("");
+const [ indicadorValor, setIndicadorValor] = useState ("");
 useEffect (() => { 
   getCharactersQuote();
 }, []);
-
 const getCharactersQuote = async () =>{
-  const res = await fetch ("https://api.gameofthronesquotes.xyz/v1/random");
+  const res = await fetch ("https://mindicador.cl/api");
   const data= await res.json();
-  setCharacterName(data.character.name)
-  setCharacterSentence(data.sentence)
+  setIndicadorNombre(data.uf.nombre)
+  setIndicadorFecha(data.uf.fecha)
+  setIndicadorValor(data.uf.valor)
   //console.log(data.character.name + " - " + data.sentence)
 }
-
 return(
 <>
-<h1>{characterName}</h1>
-<h3>{characterSentence}</h3>
+<h1>{indicadorNombre}</h1>
+<h3>{indicadorFecha}</h3>
+<h3>{indicadorValor}</h3>
 <button  className='mt-3' onClick={getCharactersQuote}>Obtener otra frase celebre de GOT</button>
 </>
 )
